6d1842d70a2cda27f7fdc6f7a6e07a8b0bf9df8b
Wanjiru Mwangi <Wanjiru Mwangi>
2017-03-06 16:46:25 +0300

    testing arithmetic options

--- arithm.py
+++ arithm.py
@@ -1,1 +1,16 @@
+def solution(x,y,op):
+	if op == "+":
+		return x+y
+	elif op == "*":
+		return x*y
+	elif op =="-":
+		return x-y
+	elif op == "/":
+		return x/y
+	elif op == "%":
+		return x%y
+
+
+
+
 
