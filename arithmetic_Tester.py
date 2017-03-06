f1e8aff1c6b2b96f1fab5fa6e879080e7d9f6a5e
Wanjiru Mwangi <Wanjiru Mwangi>
2017-03-06 16:57:52 +0300

    arithmetic test file

--- arithmetic_Tester.py
+++ arithmetic_Tester.py
@@ -4,3 +4,5 @@ from arithm import solution
 class TestSolution(unittest.TestCase):
     def test_addition(self):
         self.assertTrue(solution(10,20,"+"),30)
+    def test_subtraction(self):
+        self.assertTrue(solution(35,15,"-"),20)
